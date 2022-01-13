5b0e8b9afacc7dfaccedf1c81ba6f996d37072e0
shubhra-agrahari <shubhraagrahari11@gmail.com>
2022-01-13 18:20:32 +0530

    first commit to

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
+import Speech from 'react-speech';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+        {/* <Button className= "speek">click me</Button> */}
+       <Speech text="say i love you to your girlfriend">Make me talk
+       </Speech>
+      </header>  
     </div>
   );
 }
 
 export default App;
+
+function Button({onClick, children}){
+
+  return<button onClick={onClick} className= "speek"> {children}</button>
+}
